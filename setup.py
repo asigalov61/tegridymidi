ae562235da0ce2cfa598afa8aa50b4fe97ad764b
Alex <alexlev61@proton.me>
2024-09-06 01:54:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.system('apt-get update && apt-get install -y fluidsynth')
 
 setup(
     name="tegridymidi",
-    version="0.1.0",
+    version="0.2.0",
     description="Tegridy MIDI",
     author="Alex Lev",
     author_email="alexlev61@proton.me",
