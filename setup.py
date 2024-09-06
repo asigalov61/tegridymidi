eb7d8caad5566560fe99af7aa7c4e4c32f3b3510
Alex <alexlev61@proton.me>
2024-09-06 05:52:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tegridymidi",
-    version="0.2.2",
+    version="0.2.3",
     description="Tools for reading, writing, and manipulating MIDIs",
     long_description=long_description,
     long_description_content_type="text/markdown",
