839e055c292171e8fc73f03e93dd0b6dd050d8b3
Alex <alexlev61@proton.me>
2024-09-06 06:17:53 -0700

    Update __init__.py

--- tegridymidi/__init__.py
+++ tegridymidi/__init__.py
@@ -7,3 +7,15 @@ from .haystack_search import *
 from .midi_to_colab_audio import *
 from .files_helpers import *
 from .sample_midis_helpers import *
+
+try:
+  
+    import torch
+  
+    if torch.cuda.is_available():
+      
+      from .x_transformer_1_23_2e import *
+      import random
+      
+except ImportError:
+    pass
