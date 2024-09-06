771abb9a97222d4fb0ece892a3b38cd3bb6f3460
Alex <alexlev61@proton.me>
2024-09-06 02:01:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,19 @@ import os
 # Install fluidsynth using apt
 os.system('apt-get update && apt-get install -y fluidsynth')
 
+# Read the contents of your README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="tegridymidi",
-    version="0.2.0",
+    version="0.2.1",
     description="Tegridy MIDI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Alex Lev",
     author_email="alexlev61@proton.me",
-    packages=find_packages(),
+    packages=find_packages(),  # Automatically find and include all packages
     install_requires=[
         # List your dependencies here
     ],
