1f23deaafd13f596808d9efbfb547852d5066d18
Alex <alexlev61@proton.me>
2024-09-06 05:08:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,12 @@ setup(
     long_description_content_type="text/markdown",
     author="Alex Lev",
     author_email="alexlev61@proton.me",
+    url="https://github.com/asigalov61/tegridymidi",  # Add the URL of your project
+    project_urls={
+        "Bug Tracker": "https://github.com/asigalov61/tegridymidi/issues",
+        "Documentation": "https://github.com/asigalov61/tegridymidi/wiki",
+        "Source Code": "https://github.com/asigalov61/tegridymidi",
+    },
     packages=find_packages(),  # Automatically find and include all packages
     include_package_data=True,
     package_data={
@@ -36,6 +42,21 @@ setup(
             'cupy-cuda12x',
             'torch-summary',
         ],
-    }
-    keywords=['MIDI', 'tegridy', 'tools'],
+    },
+    keywords=['MIDI', 'tegridy', 'tools'],  # Add your keywords here
+    python_requires='>=3.6',  # Specify the Python version
+    license='Apache Software License 2.0',  # Specify the license
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',  
+        'Operating System :: OS Independent',        
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
+    ],
 )
