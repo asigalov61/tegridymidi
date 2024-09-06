168d4e010ccefc9545f1bfb5679f8df2591fefbc
Alex <alexlev61@proton.me>
2024-09-06 05:47:34 -0700

    Update __init__.py

--- tegridymidi/__init__.py
+++ tegridymidi/__init__.py
@@ -1,8 +1,11 @@
 from .tegridymidi import *
+from .constants import *
+from .matrixes import *
+from .plots import *
 from .midi import *
 from .haystack_search import *
 from .midi_to_colab_audio import *
-from .constants import *
+
 from .files_helpers import *
 from .sample_midis_helpers import *
 
